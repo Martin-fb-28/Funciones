297811bf07d3df070fff3397baa6c8d1e0051b5c
Martin-fb-28 <martinfbao@gmail.com>
2025-10-23 13:38:19 +0200

    Acabado el ejercicio de saludo

--- src/saludo.java
+++ src/saludo.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class saludo {
-    public static void main(String[] args) {
 
 
-
+    public static void saludar(String nombreEntrada){
+        System.out.println(" Bienvenido " + nombreEntrada);
     }
+
+
+    public static void main(String[] args){
+        Scanner lector = new Scanner(System.in);
+        System.out.println("¿Cuál es tu nombre?");
+        String nombre = lector.nextLine();
+
+        saludar(nombre);
+    }
+
+
+
+
+
+
+
+
 }
+
